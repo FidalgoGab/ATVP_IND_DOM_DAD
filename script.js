caff28fbb8600becfc4775206e7f581b22620bfa
FidalgoGab <99514428+FidalgoGab@users.noreply.github.com>
2022-09-14 21:26:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const maiorPais = document.getElementById('t2').getElementsByTagName('tr').getElementsByTagName('td')
+let paisao = maiorPais.length
+const divPais = document.getElementById('paisMaior')
+
+while(paisao > maiorPais.length){
+    
+}
+
